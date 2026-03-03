4a1ae0ddc90ef2a9e64237bc68a253aa053fb624
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2026-03-03 10:50:43 +0700

    menambahkan procedur untuk input data

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,3 +4,13 @@ using namespace std;
 
 int arr[20]; // deklarasi variabel array dengan ukuran 20
 int n; //deklarasi variable n 
+
+void input()
+{//procedur untuk input
+    int d;
+    while (true)
+    {
+        
+    }
+
+}
